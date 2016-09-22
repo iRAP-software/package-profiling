691d7d66033dc959b340fb1108860c04e9f302e5
Zeeshan Ahmed <ziishaned@gmail.com>
2016-09-22 18:17:14 +0500

    rename example file to index

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+/* 
+ * This is an example to demonstrate the usage of the function_analyzer class
+ */
+
+use iRAP\Profiling\FunctionLog;
+use iRAP\Profiling\FunctionAnalyzer;
+
+require_once 'vendor/autoload.php';
+
+function sleeper()
+{
+    sleep(1);
+}
+
+function Bar()
+{
+    FunctionAnalyzer::start();
+    sleeper();
+    sleeper();
+    sleeper();
+    FunctionAnalyzer::stop();
+}
+
+function Foo()
+{
+    FunctionAnalyzer::start();
+    sleeper();
+    Bar();
+    FunctionAnalyzer::stop();
+}
+
+Foo();
+echo FunctionAnalyzer::getResults();
